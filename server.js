b4280ec55b8758fdf49816c62b1930aab7dcbf64
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-10 16:17:57 -0400

    adding process.env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const htmlRoutes = require('./routes/html-routes.js')
 const apiRoutes = require('./routes/apiRoutes.js')
 app.use(express.json());
